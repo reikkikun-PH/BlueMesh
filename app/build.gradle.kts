1c8031985fccc7c75b0e87dbcb51857faf852ef3
ricamarse-max <ricamarse@gmail.com>
2026-06-13 14:02:53 +0800

    Bump version to 26.3 and compile release APKs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "26.2"
+        versionName = "26.3"
     }
 
     val keystorePropertiesFile = file("C:/Users/Ricky/Desktop/BlueMesh Creds/keystore.properties")
